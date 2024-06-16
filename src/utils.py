66bddc1ee5802b73c75bd64afe98d2ac2814510a
eng-alecrim <lucasalecrimvieira@gmail.com>
2024-06-16 20:39:04 -0300

    FIX: loop infinito -> nome do diretorio do projeto estava errado

--- src/utils.py
+++ src/utils.py
@@ -1,13 +1,28 @@
-def get_path_projeto() -> str:
+# =============================================================================
+# BIBLIOTECAS E MÓDULOS
+# =============================================================================
+
+from pathlib import Path
+
+# =============================================================================
+# CONSTANTES
+# =============================================================================
+
+NOME_PROJETO = "fiap-mle-tech-challenge"
+
+# =============================================================================
+# FUNÇÕES
+# =============================================================================
+
+
+def get_path_projeto(nome_projeto: str = NOME_PROJETO) -> str:
     """
     Retorna o caminho absoluto do diretório do projeto, subindo na estrutura de diretórios até encontrar o diretório com o nome do projeto especificado.
 
     Retorna:
     - caminho_projeto (str): Caminho absoluto do diretório do projeto.
     """
-    from pathlib import Path
 
-    nome_projeto = "tech-challenge-1"
     path_atual = Path.cwd()
 
     # Subindo na estrutura de diretórios até encontrar o diretório do projeto
