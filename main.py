5708058e031c2e8e634ab0b746f1ddb8ea1fdf56
eng-alecrim <lucasalecrimvieira@gmail.com>
2024-06-16 19:50:27 -0300

    ATT: adicionei o endpoint para fazer download dos dados .csv

--- main.py
+++ main.py
@@ -1,14 +1,55 @@
-from typing import Optional
-from fastapi import FastAPI
+# =============================================================================
+# BIBLIOTECAS E MÓDULOS
+# =============================================================================
+
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import FileResponse
+
+# =============================================================================
+# CONSTANTES
+# =============================================================================
+
+PATH_DADOS_CATEGORIAS = {
+    "comercializacao": "data/bronze/Comercio.csv",
+    "exportacao": "data/bronze/ExpVinho.csv",
+    "importacao": "data/bronze/ImpVinhos.csv",
+    "producao": "data/bronze/Producao.csv",
+    "processamento": "data/bronze/ProcessaViniferas.csv",
+}
+
+# =============================================================================
+# CLASSES
+# =============================================================================
+
+# FUTURAS CLASSES DO PYDANTIC PARA VALIDAÇÃO DE DADOS
+
+# =============================================================================
+# FUNÇÕES
+# =============================================================================
+
+# FUTURAS FUNÇÕES
+
+# =============================================================================
+# CÓDIGO
+# =============================================================================
+
+# -----------------------------------------------------------------------------
+# Aplicação
+# -----------------------------------------------------------------------------
 
 app = FastAPI()
 
+# -----------------------------------------------------------------------------
+# Endpoints
+# -----------------------------------------------------------------------------
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
 
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/dados/{categoria}")
+async def get_dados(categoria: str) -> FileResponse:
+    path_dado = PATH_DADOS_CATEGORIAS.get(categoria, None)
+    if path_dado is None:
+        raise HTTPException(
+            status_code=404,
+            detail=f"Arquivo não encontrado! Dados disponíveis: {', '.join(PATH_DADOS_CATEGORIAS.keys())}",
+        )
+    return FileResponse(path=path_dado, status_code=200)
