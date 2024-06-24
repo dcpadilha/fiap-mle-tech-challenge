b0b7e999fd1db5de8013fdcd8b33d4a22ec179fe
Gabriel Ronny <gabrielronny123@hotmail.com>
2024-06-24 00:44:28 -0300

    feat: organização em class

--- scrapping/app.py
+++ scrapping/app.py
@@ -3,65 +3,78 @@ import requests
 import re
 import json
 import utils
+from enum import Enum
 
-Production = "opt_02"
-Proccess = "opt_03"
-Commercialization = "opt_04"
-Importation = "opt_05"
-Exportation = "opt_06"
-
-base_url = "http://vitibrasil.cnpuv.embrapa.br/index.php"
-
-html_page = requests.get(f"{base_url}?opcao={Production}").text
-soup = BeautifulSoup(html_page, "html.parser")
-
-tb_datas = soup.find_all("table")[5]
-str_datas = tb_datas.find("label", attrs={"class": "lbl_pesq"}).text
-pattern = r'\[\s*(\d{4})\s*-\s*(\d{4})\s*\]'
-match = re.search(pattern, str_datas)
-
-start_date, end_date = int(match.group(1)), int(match.group(2))
-
-print(start_date, end_date)
-
-tables = soup.find_all('table', class_='tb_base tb_dados')
+class TypeOption(Enum):
+    Production = "opt_02"
+    Proccess = "opt_03"
+    Commercialization = "opt_04"
+    Importation = "opt_05"
+    Exportation = "opt_06"
 
 
-list_years = list(range(start_date, (end_date + 1)))
+class ScrappingApp():
 
-data = []
+    def __init__(self):
+        self.base_url = "http://vitibrasil.cnpuv.embrapa.br/index.php"
+        self.soup = None
+        self.html_page = None
+        self.data = None
 
-    
-current_year = start_date
+    def get_base_html(self):
+        self.html_page = requests.get(f"{self.base_url}?opcao={TypeOption.Production}").text
+        self.soup = BeautifulSoup(self.base_url, "html.parser")
 
+    def get_years_of_data(self):
+        # Responsável por coletar o range de datas
+        tb_datas = self.soup.find_all("table")[5]
+        str_datas = tb_datas.find("label", attrs={"class": "lbl_pesq"}).text
+        pattern = r'\[\s*(\d{4})\s*-\s*(\d{4})\s*\]'
+        match = re.search(pattern, str_datas)
 
-for table in tables:
-    rows = table.find_all('tr')
-    
-    current_item = None
-    for row in rows:
-        main_cell = row.find('td', class_='tb_item')
-        if main_cell:
-            item_name =  re.sub(r'^\s+|\s+$', '', main_cell.text)
+        return list(range(int(match.group(1)), (int(match.group(2) + 1))))
+        
+    def get_info_page(self):
+        tables = self.soup.find_all('table', class_='tb_base tb_dados')
+
+        for table in tables:
+            rows = table.find_all('tr')
             
-            # Se encontrar um item principal, inicializa o dicionário
-            item_name = main_cell.text
-            item_name =  re.sub(r'^\s+|\s+$', '', main_cell.text)
+            current_item = None
+            for row in rows:
+                main_cell = row.find('td', class_='tb_item')
+                if main_cell:
+                    item_name =  re.sub(r'^\s+|\s+$', '', main_cell.text)
+                    
+                    # Se encontrar um item principal, inicializa o dicionário
+                    item_name = main_cell.text
+                    item_name =  re.sub(r'^\s+|\s+$', '', main_cell.text)
+                    
+                    item_value = utils.convert_to_int(main_cell.find_next_sibling('td').text)
+
+                    current_item = {item_name: {"Valor Total": item_value, "Ano da Informação": '2024'}}
+                    self.data.append(current_item)
+                else:
+                    # Se não for item principal, processa como subitem
+                    sub_cells = row.find_all('td')
+                    if sub_cells and len(sub_cells) > 1:
+
+                        sub_item_name = re.sub(r'^\s+|\s+$', '', sub_cells[0].text)
+                        if not 'Total' in sub_item_name:
+                            sub_item_value = utils.convert_to_int(sub_cells[1].text.replace(" ", "").replace("\n","").replace(".", "")) 
+                            current_item[item_name][sub_item_name] = sub_item_value
+        self.save_json(self, "data", "Production")
+
+    def save_json(self, name_file, name_page):
+        with open(f'./data/{name_page}/{name_file}.json', 'w', encoding='utf-8') as json_file:
+            json.dump(self.data, json_file, indent=4,  ensure_ascii=False)
+
+    def run(self):
+        self.get_base_html()
+        self.get_info_page()
+        self.get_years_of_data()
+        self.get_info_page()
 
 
-            item_value = utils.convert_to_int(main_cell.find_next_sibling('td').text)
-
-            current_item = {item_name: {"Valor Total": item_value, "Ano da Informação": current_year}}
-            data.append(current_item)
-        else:
-            # Se não for item principal, processa como subitem
-            sub_cells = row.find_all('td')
-            if sub_cells and len(sub_cells) > 1:
-
-                sub_item_name = re.sub(r'^\s+|\s+$', '', sub_cells[0].text)
-                if not 'Total' in sub_item_name:
-                    sub_item_value = utils.convert_to_int(sub_cells[1].text.replace(" ", "").replace("\n","").replace(".", "")) 
-                    current_item[item_name][sub_item_name] = sub_item_value
-
-with open('data.json', 'w', encoding='utf-8') as json_file:
-    json.dump(data, json_file, indent=4,  ensure_ascii=False)
+if __name__ == '__main__':
+    ScrappingApp().run()
