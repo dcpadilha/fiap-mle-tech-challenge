b28e22322332df4c0dbfdddc81cf11f05699d4a8
Thales Gomes <thalexbr@yahoo.com.br>
2024-07-29 20:35:09 -0300

    cleaning up the code

--- embrapa-db/initialize.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [[ ! -z "${CREATE_DB}" ]]; then 
-  if [[ ${CREATE_DB} = 'true' ]]; then
-    mongoimport -d embrapa -c users /localfiles/users.json --jsonArray
-    mongoimport -d embrapa -c scrape_target /localfiles/targets.json --jsonArray
-  fi
-fi 
-
